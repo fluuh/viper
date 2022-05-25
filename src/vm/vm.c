cb13cc3fd03228d395fe390d36077225744b218d
fluuh <102534479+fluuh@users.noreply.github.com>
2022-05-25 10:55:52 +0200

    Dispatch loop isn't a switch

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -120,20 +120,19 @@ static int dispatch(vp_state *state)
 		next()
 	
 	#define vm_case(op) \
-		OPC_##op
+		OPC_##op:
 
 	#endif
 
 	LOAD_FRAME();
 
-	vm_loop {
-	vm_case(NOP):
-		vm_break;
-	vm_case(END):
-		return vperr_code; // unreachable
-	vm_case(RETVOID):
+	vm_break;
+
+	vm_case(NOP) vm_break;
+	vm_case(END) return vperr_code; // unreachable
+	vm_case(RETVOID)
 	{
-		if(fn->type.ret != vp_void) {
+		if (fn->type.ret != vp_void) {
 			return 1;
 		}
 		// there is no need to deal with return values
@@ -141,19 +140,19 @@ static int dispatch(vp_state *state)
 		LOAD_FRAME();
 		vm_break;
 	}
-	vm_case(RET):
+	vm_case(RET)
 	{
 		imm();
 		u8 rreg = imm();
 		void *ret;
-		switch(fn->type.ret) {
-		case(vp_i32):
+		switch (fn->type.ret) {
+		case (vp_i32):
 			ret = r32 + rreg;
 			break;
-		case(vp_i64):
+		case (vp_i64):
 			ret = r64 + rreg;
 			break;
-		case(vp_iarch):
+		case (vp_iarch):
 			ret = rxx + rreg;
 			break;
 		default:
@@ -161,15 +160,15 @@ static int dispatch(vp_state *state)
 			ret = 0;
 			break;
 		}
-		if(state->fp == state->frames) {
+		if (state->fp == state->frames) {
 			// there are no frames left,
 			// return to host
 			// the host will use this to find the return value
-			state->fp->ret_offset = 
+			state->fp->ret_offset =
 			    (size_t)((uxx)ret - (uxx)state->fp);
 			return 0;
 		}
-		if(state->fp->ret_offset == 0) {
+		if (state->fp->ret_offset == 0) {
 			// the return value doesn't matter
 			pop_callframe(state);
 			vm_break;
@@ -177,18 +176,18 @@ static int dispatch(vp_state *state)
 		// save return value
 		pop_callframe(state);
 
-		switch(fn->type.ret) {
-		case(vp_i32):
-			*(u32*)((u8*)state->fp + state->fp->ret_offset) =
-			    *(u32*)ret; 
+		switch (fn->type.ret) {
+		case (vp_i32):
+			*(u32 *)((u8 *)state->fp + state->fp->ret_offset) =
+			    *(u32 *)ret;
 			break;
-		case(vp_i64):
-			*(u64*)((u8*)state->fp + state->fp->ret_offset) =
-			    *(u64*)ret; 
+		case (vp_i64):
+			*(u64 *)((u8 *)state->fp + state->fp->ret_offset) =
+			    *(u64 *)ret;
 			break;
-		case(vp_iarch):
-			*(uxx*)((u8*)state->fp + state->fp->ret_offset) =
-			    *(uxx*)ret; 
+		case (vp_iarch):
+			*(uxx *)((u8 *)state->fp + state->fp->ret_offset) =
+			    *(uxx *)ret;
 			break;
 		default:
 			// do nothing
@@ -197,54 +196,32 @@ static int dispatch(vp_state *state)
 		LOAD_FRAME();
 		vm_break;
 	}
-	vm_case(JMP):
-		// TODO: OP_JMP
-		return vperr_internal;
-	vm_case(NEG_W):
-		return vperr_internal;
-	vm_case(NEG_L):
-		return vperr_internal;
-	vm_case(EQZ_W):
-		return vperr_internal;
-	vm_case(EQZ_L):
-		return vperr_internal;
-	vm_case(EQ_W):
-		return vperr_internal;
-	vm_case(EQ_L):
-		return vperr_internal;
-	vm_case(LT_WU):
-		return vperr_internal;
-	vm_case(LT_LU):
-		return vperr_internal;
-	vm_case(LT_WS):
-		return vperr_internal;
-	vm_case(LT_LS):
-		return vperr_internal;
-	vm_case(GT_WU):
-		return vperr_internal;
-	vm_case(GT_LU):
-		return vperr_internal;
-	vm_case(GT_WS):
-		return vperr_internal;
-	vm_case(GT_LS):
-		return vperr_internal;
-	vm_case(LE_WU):
-		return vperr_internal;
-	vm_case(LE_LU):
-		return vperr_internal;
-	vm_case(LE_WS):
-		return vperr_internal;
-	vm_case(LE_LS):
-		return vperr_internal;
-	vm_case(GE_WU):
-		return vperr_internal;
-	vm_case(GE_LU):
-		return vperr_internal;
-	vm_case(GE_WS):
-		return vperr_internal;
-	vm_case(GE_LS):
-		return vperr_internal;
-	vm_case(ADD_W):
+	vm_case(JMP)
+	     // TODO: OP_JMP
+	      return vperr_internal;
+	vm_case(NEG_W) return vperr_internal;
+	vm_case(NEG_L) return vperr_internal;
+	vm_case(EQZ_W) return vperr_internal;
+	vm_case(EQZ_L) return vperr_internal;
+	vm_case(EQ_W) return vperr_internal;
+	vm_case(EQ_L) return vperr_internal;
+	vm_case(LT_WU) return vperr_internal;
+	vm_case(LT_LU) return vperr_internal;
+	vm_case(LT_WS) return vperr_internal;
+	vm_case(LT_LS) return vperr_internal;
+	vm_case(GT_WU) return vperr_internal;
+	vm_case(GT_LU) return vperr_internal;
+	vm_case(GT_WS) return vperr_internal;
+	vm_case(GT_LS) return vperr_internal;
+	vm_case(LE_WU) return vperr_internal;
+	vm_case(LE_LU) return vperr_internal;
+	vm_case(LE_WS) return vperr_internal;
+	vm_case(LE_LS) return vperr_internal;
+	vm_case(GE_WU) return vperr_internal;
+	vm_case(GE_LU) return vperr_internal;
+	vm_case(GE_WS) return vperr_internal;
+	vm_case(GE_LS) return vperr_internal;
+	vm_case(ADD_W)
 	{
 		u32 *dst = (r32 + imm());
 		u32 *op0 = (r32 + imm());
@@ -252,7 +229,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 + *op1;
 		vm_break;
 	}
-	vm_case(ADD_L):
+	vm_case(ADD_L)
 	{
 		u64 *dst = (r64 + imm());
 		u64 *op0 = (r64 + imm());
@@ -260,7 +237,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 + *op1;
 		vm_break;
 	}
-	vm_case(SUB_W):
+	vm_case(SUB_W)
 	{
 		u32 *dst = (r32 + imm());
 		u32 *op0 = (r32 + imm());
@@ -268,7 +245,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 - *op1;
 		vm_break;
 	}
-	vm_case(SUB_L):
+	vm_case(SUB_L)
 	{
 		u64 *dst = (r64 + imm());
 		u64 *op0 = (r64 + imm());
@@ -276,7 +253,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 - *op1;
 		vm_break;
 	}
-	vm_case(MUL_W):
+	vm_case(MUL_W)
 	{
 		u32 *dst = (r32 + imm());
 		u32 *op0 = (r32 + imm());
@@ -284,7 +261,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 * *op1;
 		vm_break;
 	}
-	vm_case(MUL_L):
+	vm_case(MUL_L)
 	{
 		u64 *dst = (r64 + imm());
 		u64 *op0 = (r64 + imm());
@@ -292,7 +269,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 * *op1;
 		vm_break;
 	}
-	vm_case(DIV_WU):
+	vm_case(DIV_WU)
 	{
 		u32 *dst = (r32 + imm());
 		u32 *op0 = (r32 + imm());
@@ -300,7 +277,7 @@ static int dispatch(vp_state *state)
 		*dst = *op0 / *op1;
 		vm_break;
 	}
-	vm_case(DIV_LU):
+	vm_case(DIV_LU)
 	{
 		u64 *dst = (r64 + imm());
 		u64 *op0 = (r64 + imm());
@@ -308,23 +285,23 @@ static int dispatch(vp_state *state)
 		*dst = *op0 / *op1;
 		vm_break;
 	}
-	vm_case(DIV_WS):
+	vm_case(DIV_WS)
 	{
-		i32 *dst = (i32*)(r32 + imm());
-		i32 *op0 = (i32*)(r32 + imm());
-		i32 *op1 = (i32*)(r32 + imm());
+		i32 *dst = (i32 *)(r32 + imm());
+		i32 *op0 = (i32 *)(r32 + imm());
+		i32 *op1 = (i32 *)(r32 + imm());
 		*dst = *op0 / *op1;
 		vm_break;
 	}
-	vm_case(DIV_LS):
+	vm_case(DIV_LS)
 	{
-		i64 *dst = (i64*)(r64 + imm());
-		i64 *op0 = (i64*)(r64 + imm());
-		i64 *op1 = (i64*)(r64 + imm());
+		i64 *dst = (i64 *)(r64 + imm());
+		i64 *op0 = (i64 *)(r64 + imm());
+		i64 *op1 = (i64 *)(r64 + imm());
 		*dst = *op0 / *op1;
 		vm_break;
 	}
-	vm_case(OBJ):
+	vm_case(OBJ)
 	{
 		u8 dst = imm();
 		u32 id = imm4();
@@ -332,35 +309,34 @@ static int dispatch(vp_state *state)
 		*(rxx + dst) = val;
 		vm_break;
 	}
-	vm_case(LDI_W):
+	vm_case(LDI_W)
 	{
 		u8 dst = imm();
 		u32 val = imm4();
 		*(r32 + dst) = val;
 		vm_break;
 	}
-	vm_case(BR):
-		return 1;
-	vm_case(LDI_L):
+	vm_case(BR) return 1;
+	vm_case(LDI_L)
 	{
 		u8 dst = imm();
 		u64 val = imm8();
 		*(r64 + dst) = val;
 		vm_break;
 	}
-	vm_case(CALL):
+	vm_case(CALL)
 	{
 		vp_type tdst = imm();
 		u8 dst = imm();
 		size_t rof = 0;
-		switch(tdst) {
-		case(vp_i32):
+		switch (tdst) {
+		case (vp_i32):
 			rof = state->fp->r32 + dst;
 			break;
-		case(vp_i64):
+		case (vp_i64):
 			rof = state->fp->r64 + dst;
 			break;
-		case(vp_iarch):
+		case (vp_iarch):
 			rof = state->fp->rxx + dst;
 			break;
 		default:
@@ -378,19 +354,19 @@ static int dispatch(vp_state *state)
 		int rw = 0;
 		int rl = 0;
 		int rx = 0;
-		for(int i = 0; i < num_args * 2; i++) {
+		for (int i = 0; i < num_args * 2; i++) {
 			vp_type type = *(args + i++);
 			u8 reg = *(args + i++);
-			switch(type) {
-			case(vp_i32):
+			switch (type) {
+			case (vp_i32):
 				*(VP_R32(state->fp) + rw) = *(r32 + reg);
 				rw++;
 				break;
-			case(vp_i64):
+			case (vp_i64):
 				*(VP_R64(state->fp) + rl) = *(r64 + reg);
 				rl++;
 				break;
-			case(vp_iarch):
+			case (vp_iarch):
 				*(VP_RXX(state->fp) + rx) = *(rxx + reg);
 				rx++;
 				break;
@@ -398,7 +374,7 @@ static int dispatch(vp_state *state)
 				return 1;
 			}
 		}
-		if(f->ftype == vp_func_normal) {
+		if (f->ftype == vp_func_normal) {
 			LOAD_FRAME();
 		} else {
 			f->native(state);
@@ -406,8 +382,7 @@ static int dispatch(vp_state *state)
 		}
 		vm_break;
 	}
-	}
-	#undef vm_case
+#undef vm_case
 	#undef vm_break
 	#undef vm_loop
 	#undef next
