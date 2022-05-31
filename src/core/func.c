c4989716d7db2aa6c3fbd46cac9f61748a5204c9
fluuh <102534479+fluuh@users.noreply.github.com>
2022-05-31 21:57:57 +0200

    oopsie

--- src/core/func.c
+++ src/core/func.c
@@ -6,50 +6,6 @@
 
 #include <string.h>
 
-<<<<<<< HEAD
-#include <viper/core.h>
-#include <viper/util.h>
-
-vp_func *vp_func_create(vp_type ret, vp_type *args,
-                        size_t num_args)
-{
-	vp_func *fn = vu_malloc(sizeof(*fn));
-	// the code is provided by the caller
-	fn->ftype = vp_func_normal;
-	fn->cap_code = 0;
-	fn->size_code = 0;
-	fn->code = (void *)0;
-	fn->type.ret = ret;
-	fn->type.num_args = num_args;
-	memcpy(fn->type.args, args, num_args);
-	return fn;
-}
-
-vp_func *vp_func_create_native(vp_type ret, vp_type *args,
-                               size_t num_args, vp_native_func func)
-{
-	vp_func *fn = vu_malloc(sizeof(*fn));
-	/* unused fields */
-	fn->code = (void *)0;
-	fn->ftype = vp_func_native;
-	fn->native = func;
-	fn->type.ret = ret;
-	fn->type.num_args = num_args;
-	memcpy(fn->type.args, args, num_args);
-	return fn;
-}
-
-int vp_func_free(vp_func *fn)
-{
-	if (fn->code != (void *)0) {
-		vu_free(fn->code);
-	}
-	if (fn->name != (void *)0) {
-		vu_free(fn->name);
-	}
-	vu_free(fn);
-	return 0;
-=======
 #include "core.h"
 
 struct vi_code *vi_code_create(int nreg, size_t size, u8 *code)
@@ -82,5 +38,4 @@ void vi_func_delete(struct vi_func *fn)
 void vi_func_free(struct vi_func *fn)
 {
 	vi_free(fn);
->>>>>>> bb5c964 (fuck it, full rewrite)
 }
--- src/core/obj.c
+++ src/core/obj.c
@@ -6,24 +6,6 @@
 
 #include <string.h>
 
-<<<<<<< HEAD
-#include <viper/core.h>
-#include <viper/util.h>
-
-vp_obj *vp_obj_create(u8 *init, size_t size)
-{
-	vp_obj *obj = vu_malloc(sizeof(*obj) + size);
-	obj->id = 0; // this is set by the caller
-	obj->size = size;
-	memcpy(obj->init, init, size);
-	return obj;
-}
-
-int vp_obj_free(vp_obj *obj)
-{
-	vu_free(obj);
-	return 0;
-=======
 #include "core.h"
 
 struct vi_obj *vi_obj_create(size_t size, u8 *initial)
@@ -38,5 +20,4 @@ struct vi_obj *vi_obj_create(size_t size, u8 *initial)
 void vi_obj_delete(struct vi_obj *obj)
 {
 	vi_free(obj);
->>>>>>> bb5c964 (fuck it, full rewrite)
 }
