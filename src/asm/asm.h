16ac4e06649092c0f1941f28648f6a0baae5a04c
fluuh <102534479+fluuh@users.noreply.github.com>
2022-06-14 23:15:10 +0200

    Move parser structs from asm.h to parser.h

--- src/asm/asm.h
+++ src/asm/asm.h
@@ -7,44 +7,8 @@
 #ifndef VP_ASM_H_
 #define VP_ASM_H_
 
-#include <viper/emitter.h>
 #include <viper/asm.h>
 #include <viper/viper.h>
-
-#define ASM_MAX_LABELS 256
-#define ASM_MAX_OPERANDS 18 /* memory usage go poof */
-#define ASM_MAX_INSTS 1024  /* seriously, fuck memory */
-
-/* could use vi_obj, but I don't want to depend on core too much */
-struct asm_obj {
-	char *name;
-	size_t size;
-	u8 initial[];
-};
-
-struct asm_inst {
-	int len;
-	int op;
-	vp_value args[];
-};
-
-struct asm_code {
-	int num_labels;
-	int labels[ASM_MAX_LABELS]; /* instruction index */
-	int num_insts;
-	struct asm_inst insts[ASM_MAX_INSTS];
-};
-
-struct asm_func {
-	enum {
-		func_private,
-		func_exported,
-		func_imported,
-	} ty;
-	int id;
-	char *name;
-	struct asm_code code;
-};
 
 struct vi_asm {
 	const char *src;
---
+++ src/asm/parser.h
@@ -0,0 +1,67 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef VI_ASM_PARSER_H_
+#define VI_ASM_PARSER_H_
+
+#include <stdlib.h>
+
+#define ASM_MAX_LABELS 256
+#define ASM_MAX_OPERANDS 18 /* memory usage go poof */
+
+/* could use vi_obj, but I don't want to depend on core too much */
+struct asm_obj {
+	char *name;
+	size_t size;
+	u8 initial[];
+};
+
+struct asm_operand {
+	enum {
+		asm_reg,
+		asm_num,
+		asm_name,
+		asm_label,
+	};
+	union {
+		i64 num;
+		char *str;
+	};
+};
+
+struct asm_inst {
+	struct asm_inst *next;
+	struct asm_inst *prev;
+	int op;
+	int len;
+	struct asm_operand args[];
+};
+
+struct asm_label {
+	char *name;
+	int pos;
+};
+
+struct asm_code {
+	int num_labels;
+	struct asm_label labels[ASM_MAX_LABELS]; /* instruction index */
+	int num_insts;
+	struct asm_inst *inst;
+};
+
+struct asm_func {
+	enum {
+		func_private,
+		func_exported,
+		func_imported,
+	} ty;
+	int id;
+	char *name;
+	struct asm_code code;
+};
+
+
+#endif
