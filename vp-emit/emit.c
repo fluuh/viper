acf2d3d3c3fb0d45c6c0d44366e8d3f8c2ce626e
fluuh <fluhuh@proton.me>
2022-07-08 10:06:14 +0200

    include correct header in emit.c

--- vp-emit/emit.c
+++ vp-emit/emit.c
@@ -1,19 +1,29 @@
-#include "vp.h"
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "emit.h"
 
 /* we can't depend on the definitions in viper */
 
-#define OPCODE(ENUM, NAME, TYPE) sizeof(TYPE),
-static const unsigned char bc_length[] = {BCDEF(OPCODE)};
+#define OPCODE(ENUM, NAME, TYPE) sizeof(TYPE) ,
+static const unsigned char bc_length[] = {
+	BCDEF(OPCODE)
+};
 #undef OPCODE
 
-#define OPCODE(ENUM, NAME, TYPE) TYPE,
-static const char *const bc_type[] = {BCDEF(OPCODE)};
+#define OPCODE(ENUM, NAME, TYPE) TYPE ,
+static const char *const bc_type[] = {
+	BCDEF(OPCODE)
+};
 #undef OPCODE
 
 static vpe_insn *create_insn(vp_bc code)
 {
-	vpe_insn *insn =
-	    vmalloc(sizeof(*insn) + sizeof(*insn->ops) * bc_length[code]);
+	vpe_insn *insn = vmalloc(sizeof(*insn) +
+		                 sizeof(*insn->ops) * bc_length[code]);
 	insn->code = code;
 	insn->n_ops = bc_length[code];
 	insn->next = NULL;
@@ -24,7 +34,7 @@ static vpe_insn *emit(vpe_function *func, int code)
 {
 	vpe_block *block = func->code.last;
 	vpe_insn *insn = create_insn(code);
-	if (block->first == NULL) {
+	if(block->first == NULL) {
 		block->first = insn;
 	} else {
 		block->last->next = insn;
@@ -37,3 +47,4 @@ vpe_insn *vpe_insn_nop(vpe_function *func)
 {
 	return emit(func, BC_NOP);
 }
+
